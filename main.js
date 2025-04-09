a5eec867a57e9823f19baf9464bf742c5a9bd58f
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-04-09 11:25:00 +0100

    added an additional holiday and fixed a mismatch between the intended and actual behaviour of the software

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const christmas = {
-  date: "2025-12-25",
-  elementId: "my-element",
-};
-
-const newYear = {
-  date: "2026-01-01",
-  elementId: "my-element2",
-};
+const holidaysData = [
+  {
+    date: "2025-12-25",
+    elementId: "my-element",
+  },
+  {
+    date: "2026-01-01",
+    elementId: "my-element2",
+  },
+  {
+    date: "2025-02-14",
+    elementId: "my-element3",
+  },
+];
 
 function displayDaysUntilHoliday(holiday) {
-
   var daysUntilHoliday = countDaysUntilHoliday(holiday.date);
 
   var elementReference = document.getElementById(holiday.elementId);
@@ -20,9 +24,22 @@ function displayDaysUntilHoliday(holiday) {
 function countDaysUntilHoliday(date) {
   var todayDate = new Date();
   var holidayDate = new Date(date);
+  var millisecondsInDay = 1000 * 60 * 60 * 24;
 
-  return Math.round((holidayDate - todayDate) / (1000 * 60 * 60 * 24));
+  if (checkIfHolidayDateInPast(todayDate, holidayDate)) {
+    holidayDate = fixOldDateToFutureDate(holidayDate);
+  }
+  return Math.round((holidayDate - todayDate) / millisecondsInDay);
 }
 
-displayDaysUntilHoliday(christmas);
-displayDaysUntilHoliday(newYear);
+function checkIfHolidayDateInPast(todayDate, holidayDate) {
+  return new Date(todayDate) > new Date(holidayDate);
+}
+
+function fixOldDateToFutureDate(oldHolidayDate) {
+  return oldHolidayDate.setFullYear(oldHolidayDate.getFullYear() + 1);
+}
+
+holidaysData.forEach((holiday) => {
+  displayDaysUntilHoliday(holiday);
+});
